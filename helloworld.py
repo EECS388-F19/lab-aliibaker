443ecb5ebf2e4c930ca624f4f4e0fe2c8dd4beb3
aliibaker <alibaker@0587399388.wireless.umich.net>
2019-09-09 16:41:49 -0400

    Pushing some code

---
+++ helloworld.py
@@ -0,0 +1,30 @@
+import random
+
+x = random.randint(0,100)
+y = random.randint(0,100)
+
+print("Ali")
+
+print(x)
+print(y)
+
+print("Sum = " + str((x+y)))
+print("Average = " + str((x+y/2)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
